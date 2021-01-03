e1ed0e790e4f9bf84de7a97e3c1be7c1892476e3
Tushar Garud <tushar.garud@outlook.com>
2021-01-02 19:56:15 -0500

    Added all files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 
@@ -8,9 +8,11 @@ api = Api(app)
 
 class TestResource(Resource):
     def get(self):
-        return 'Haha'
-
+        status = request.args.get('status')
+        switch_number = request.args.get('switch_number')
+        duration = request.args.get('duration')
+        return "Hello"
 
 api.add_resource(TestResource, "/test")
 
-app.run(host='0.0.0.0', port=5005)
+app.run(host='0.0.0.0', port=5005, debug=True)
--- halloween.py
+++ halloween.py
@@ -1,46 +1,51 @@
+#!/home/pi/virtual_envs/controller_venv/bin/python3
+
 import RPi.GPIO as GPIO
 import time
 from flask import Flask, render_template, request
 app = Flask(__name__)
-zombie = 40
-stonewolf = 38
-werewolf = 36
-pumpkin = 32
-monstermash = [40, 38, 36, 32]
+HEAD_UP = 38
+HEAD_DOWN = 40
+LEG_UP = 32
+LEG_DOWN = 36
+pins_used = [HEAD_UP, HEAD_DOWN, LEG_UP, LEG_DOWN]
 GPIO.setmode(GPIO.BOARD)
-relay = monstermash
-for x in relay:
+DEFAULT_POSITION = HEAD_DOWN
+DEFAULT_DURATION = 5
+for x in pins_used:
     GPIO.setup(x, GPIO.OUT)
+    GPIO.output(x, GPIO.LOW)
 
 @app.route("/")
 def index():
     return render_template('index.html')
-@app.route("/<deviceName>/")
-def action(deviceName):
-    if deviceName != 'monstermash':
-        if deviceName == 'stonewolf':
-            relay = stonewolf
-        if deviceName == 'werewolf':
-            relay = werewolf
-        if deviceName == 'pumpkin':
-            relay = pumpkin
-        if deviceName == 'zombie':
-            relay = zombie
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(relay, GPIO.OUT)
-        GPIO.output(relay, GPIO.LOW)
-        time.sleep(5)
+
+@app.route("/bedController")
+def action():
+    bed_part = request.args.get('bed_part')
+    bed_position = request.args.get('bed_position')
+    duration = int(request.args.get('duration'))
+    # resp = f"bed_part={bed_part} bed_position={bed_position} duration={duration}"
+    # print(resp)
+    relay = DEFAULT_POSITION
+    if duration is None or duration < 0 or duration > 20:
+        duration = DEFAULT_DURATION
+    if bed_part is not None and bed_position is not None:
+        if bed_part.lower() == "head":
+            if bed_position.lower() == "up":
+                relay = HEAD_UP
+            else:
+                relay = HEAD_DOWN
+        elif bed_part.lower() == "leg":
+            if bed_position.lower() == "up":
+                relay = LEG_UP
+            else:
+                relay = LEG_DOWN
         GPIO.output(relay, GPIO.HIGH)
-    else:
-        relay = monstermash
-        for x in relay:
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(x, GPIO.OUT)
-            GPIO.output(x, GPIO.LOW)
-        time.sleep(5)
-        for x in relay:
-            GPIO.output(x, GPIO.HIGH)
+        time.sleep(duration)
+        GPIO.output(relay, GPIO.LOW)
     return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5005, debug=True)
     
